e9f874737dc4432f3abf38538cbe013a0281d218
nuzulzen <nuzulzen@gmail.com>
2020-10-07 01:47:38 +0700

    fx wide recipes and bgColor

--- src/Recipes.js
+++ src/Recipes.js
@@ -6,12 +6,15 @@ const Recipe = ({title, calories, image, ingredients}) =>{
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
-            <p>{calories}</p>
-            <ol>
-              {ingredients.map(ingredients =>(
-                  <li> {ingredients.text} </li>
-              ))}
-            </ol>
+            <p className={style.calories} > <strong>Total Calories {Math.ceil(calories)}</strong> </p>
+            <div className={style.list__recipes}>
+                <ol className="list__recipe">
+                {ingredients.map(ingredients =>(
+                    <li> {ingredients.text} </li>
+                ))}
+                </ol>
+
+            </div>
             <img className={style.image} src={image} alt=""/>
         </div>
     )
